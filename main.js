8563f2585340a49cbc2a2549b48e878e9e28bf7b
TheBest-Coin <theBcoin.io@gmail.com>
2024-12-25 22:00:45 +0330

    Delete main.js

--- main.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener('load', function() {
-    // زمانی که تمام محتوا بارگذاری شد
-    const loadingScreen = document.getElementById('loading-screen');
-    const mainContent = document.getElementById('main-content');
-
-    // مخفی کردن صفحه لودینگ و نمایش محتوای اصلی
-    loadingScreen.style.display = 'none';
-    mainContent.style.display = 'block';
-});

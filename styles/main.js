9865f726f87c8eb4f01da1c2a91812c1ca6300d2
TheBest-Coin <theBcoin.io@gmail.com>
2025-02-02 01:38:58 +0330

    Add files via upload

---
+++ styles/main.js
@@ -0,0 +1,29 @@
+let a = document.getElementById('coinImage');
+let r = document.getElementById('tokenCount');
+let RcoinP = document.getElementById('Rcoin-Pargraph');
+
+let Rcoin = localStorage.getItem("Rcoin");
+RcoinP.innerText = Rcoin;
+
+let plusNum = localStorage.getItem('plusValue');
+let SpanViewPlus = document.getElementById('plus-s');
+let b = localStorage.getItem('Vcoin');
+r.innerText = b;
+a.addEventListener('click' , () =>{
+    let Cb = localStorage.getItem('plusValue');
+
+    if (Cb >= 1){
+        b ++;
+    r.innerText = b;
+    localStorage.setItem('Vcoin',b);
+    plusValue --;
+    SpanViewPlus.innerHTML = plusValue;
+    localStorage.setItem ('plusValue', plusValue);
+
+    }else{
+        window.location.href = "./pages/charg.html";
+    }
+
+
+});
+

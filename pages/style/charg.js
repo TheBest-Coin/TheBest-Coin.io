fbe8e90b1fbd640737c5afe473bdbae37057a02b
TheBest-Coin <theBcoin.io@gmail.com>
2025-02-09 20:46:24 +0330

    Update charg.js

--- pages/style/charg.js
+++ pages/style/charg.js
@@ -7,16 +7,17 @@ document.getElementById('BackBtn').addEventListener('click',()=>{
     window.location.href="../index.html";
 });
 
-let BtnShoed = document.getElementById('Pluser2')
+let BtnShoed = document.getElementById('Pluser')
 let chargVal = localStorage.getItem('plusValue')
 pluserpar.innerText=chargVal;
+
 BtnShoed?.addEventListener('click', ()=>{
-    window.TelegramAdsController.triggerNativeNotification().then((result) =>{
+     window.TelegramAdsController.triggerNativeNotification().then((result) =>{
     B++;
     LocalR = localStorage.setItem("Rcoin",B);
     plusValue += 15;
     localStorage.setItem('plusValue',plusValue);
     pluserpar.innerText=plusValue;
-    }).catch((result)=>{
-    });
+     }).catch((result)=>{
+     });
 });
